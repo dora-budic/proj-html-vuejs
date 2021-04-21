9f65e31217bab8772184a8f79983b89046074270
Dora Budić <dbudic0706@gmail.com>
2021-04-21 15:18:32 +0200

    Header-nav bottom

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,41 @@ Vue.config.devtools = true;
 var app = new Vue ({
   el: '#root',
   data: {
-
+    contacts: [
+      {
+        family: 'fas',
+    		prefix: 'fa-',
+    		type: 'phone-alt',
+        text: '+1 (305) 1234-5678',
+      },
+      {
+        family: 'fas',
+        prefix: 'fa-',
+        type: 'envelope',
+        text: 'hello@example.com',
+      },
+      {
+        family: 'fab',
+        prefix: 'fa-',
+        type: 'facebook-f',
+        text: '',
+      },
+      {
+        family: 'fab',
+        prefix: 'fa-',
+        type: 'twitter',
+        text: '',
+      },
+      {
+        family: 'fab',
+        prefix: 'fa-',
+        type: 'linkedin-in',
+        text: '',
+      }
+    ],
+    navLinks: ['home','about','services','team','blog'],
   },
   methods: {
-    
+
   }
 });
